5237044853bf3831cbeec60e15a2cc2b08ff7029
Beatrix Austria <baustria2108@gmail.com>
2021-09-04 16:55:18 +1000

    basic css done (buttons link to homepage/login)

--- mern/client/src/pages/registration/Registration.js
+++ mern/client/src/pages/registration/Registration.js
@@ -8,37 +8,56 @@ const Registration = () => {
   return (
     <React.Fragment>
       <div className="reg-parent">
-        <h1>Register on the Testing System</h1>
-        <h3>Access your account or get started with us</h3>
-        <Button variant="contained" color="primary" href="login">
-          I have an account
-        </Button>
-        <form  noValidate autoComplete="off">
-          <div>
+        <h1 className="Title">
+          Register on the Testing System
+        </h1>
+
+        <h3 className="SecondTitle">
+          Access your account or get started with us
+        </h3>
+
+        <div className="LoginButton" >
+          <Button
+                  variant="contained" 
+                  color="primary" 
+                  href="login">
+                    I have an account
+          </Button>
+        </div>
+        
+        <form className="Form" noValidate autoComplete="off">
+          <div className="Section">
+
             <TextField 
                id="clientName"
                label="Full name"
              />
           </div>
 
-          <div>
+          <div className="Section">
             <TextField 
                id="clientEmail"
                label="Email"
              />
           </div>
 
-          <div>
+          <div className="Section">
             <TextField 
                id="clientName"
                type="password"
                label="Password"
              />
           </div>
+           
+          <div className="RegisterButton">
+            <Button
+              variant="contained" 
+              color="primary" 
+              href="homepage">
+                Create Account
+            </Button>
+          </div>
 
-          <Button variant="contained" color="primary" href="homepage">
-            Create Account
-          </Button>
         </form>
       </div>
     </React.Fragment>
