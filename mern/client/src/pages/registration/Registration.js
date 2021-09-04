7a100c27b04be1d170da87071c867f62b08a7809
Beatrix Austria <baustria2108@gmail.com>
2021-09-04 14:44:41 +1000

    basic registration page (no css added yet)

--- mern/client/src/pages/registration/Registration.js
+++ mern/client/src/pages/registration/Registration.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, TextField } from "@material-ui/core"
 
 import "./Registration.css";
 
@@ -7,7 +8,38 @@ const Registration = () => {
   return (
     <React.Fragment>
       <div className="reg-parent">
-        <p> Registration ... To be completed </p>
+        <h1>Register on the Testing System</h1>
+        <h3>Access your account or get started with us</h3>
+        <Button variant="contained" color="primary" href="login">
+          I have an account
+        </Button>
+        <form  noValidate autoComplete="off">
+          <div>
+            <TextField 
+               id="clientName"
+               label="Full name"
+             />
+          </div>
+
+          <div>
+            <TextField 
+               id="clientEmail"
+               label="Email"
+             />
+          </div>
+
+          <div>
+            <TextField 
+               id="clientName"
+               type="password"
+               label="Password"
+             />
+          </div>
+
+          <Button variant="contained" color="primary" href="homepage">
+            Create Account
+          </Button>
+        </form>
       </div>
     </React.Fragment>
   );
