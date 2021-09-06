abe771b8f1afd38536e45ba59efac64bce863dbe
Homer Martillano <13537675Z@student.uts.edu.au>
2021-09-07 00:06:32 +1000

    version:1, basic login + css

--- mern/client/src/pages/login/Login.js
+++ mern/client/src/pages/login/Login.js
@@ -7,10 +7,40 @@ const Login = () => {
   return (
     <React.Fragment>
       <div className="login-parent">
-        <p> Login ... To be completed </p>
+        <h1 classname="Title">
+          Login to Testing System
+        </h1>
+
+        <form className="Form" noValidate autoComplete="off">
+          <div className="Section">
+            <TextField 
+               id="clientEmail"
+               label="Email"
+             />
+          </div>
+
+          <div className="Section">
+            <TextField 
+               id="clientPassword"
+               type="password"
+               label="Password"
+             />
+          </div>
+           
+          <div className="LoginButton">
+            <Button
+              variant="contained" 
+              color="primary" 
+              href="homepage">
+                Login
+            </Button>
+          </div>
+        </form>
+
       </div>
     </React.Fragment>
   );
+
 };
 
 export default Login;
