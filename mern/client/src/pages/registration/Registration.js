076ba164edb4edb0d11a71aab0232a9cf058643d
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2021-09-13 11:08:39 +1000

    registration styling changes to make the page more responsive

--- mern/client/src/pages/registration/Registration.js
+++ mern/client/src/pages/registration/Registration.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { ButtonGroup, Form, Image } from "react-bootstrap";
+import {
+  ButtonGroup,
+  Form,
+  Image,
+  Container,
+  Col,
+  Row
+} from "react-bootstrap";
 import ButtonContained from "../../components/ButtonContained";
 import ButtonOutlined from "../../components/ButtonOutlined";
-import logo from "./logo_register.png";
+import logo from "../../assets/logo_register.png";
 import "./Registration.css";
 
 
@@ -12,83 +19,92 @@ const Registration = () => {
     <React.Fragment>
       <div className="reg-parent">
 
-        {/*Title of Page*/}
-        <h3 className="Title">
-          Register on the Testing System
-        </h3>
+        <Container>
+          <Row>
+            <Col md={7} sm={12} xs={12}>
 
-        {/*Sub header*/}
-        <h5 className="SecondTitle">
-          Access your account or get started with us
-        </h5>
+              {/*Title of Page*/}
+              <h3 className="Title">
+                Register on the Testing System
+              </h3>
 
-        {/*Buttons*/}
-        <ButtonGroup className="LoginPageButton">
+              {/*Sub header*/}
+              <h5 className="SecondTitle">
+                Access your account or get started with us
+              </h5>
 
-          {/*Login Button, links to Login Page*/}
-          <ButtonOutlined
-            variant="contained"
-            color="primary"
-            href="login">
-            I have an account
-          </ButtonOutlined>
-        </ButtonGroup>
+              {/*Buttons*/}
+              <ButtonGroup className="LoginPageButton">
 
-        <ButtonGroup className="RegisterPageButton">
-          {/*Registration Button, links to Register Page???*/}
-          <ButtonContained
-            variant="contained"
-            color="primary"
-            href="registration">
-            I want to register
-          </ButtonContained>
+                {/*Login Button, links to Login Page*/}
+                <ButtonOutlined
+                  variant="contained"
+                  color="primary"
+                  href="login">
+                  I have an account
+                </ButtonOutlined>
+              </ButtonGroup>
 
-        </ButtonGroup>
+              <ButtonGroup className="RegisterPageButton">
+                {/*Registration Button, links to Register Page???*/}
+                <ButtonContained
+                  variant="contained"
+                  color="primary"
+                  href="registration">
+                  I want to register
+                </ButtonContained>
 
-        <Image src={logo}className="DrawingLogin"/>
+              </ButtonGroup>
 
-        {/*Registration Form*/}
-        <Form noValidate autoComplete="off">
+              {/*Registration Form*/}
+              <Form noValidate autoComplete="off">
 
-          {/*Full Name*/}
-          <Form.Group className="Section1" controlID="Name">
-            <Form.Control
-              type="text"
-              size="lg"
-              placeholder="Full name: e.g. John Smith"
-            />
-          </Form.Group>
+                {/*Full Name*/}
+                <Form.Group className="Section1" controlID="Name">
+                  <Form.Control
+                    type="text"
+                    size="lg"
+                    placeholder="Full name: e.g. John Smith"
+                  />
+                </Form.Group>
 
-          {/*Email*/}
-          <Form.Group className="Section2" controlID="Email">
-            <Form.Control
-              type="text"
-              size="lg"
-              placeholder="Email address"
-            />
-          </Form.Group>
+                {/*Email*/}
+                <Form.Group className="Section2" controlID="Email">
+                  <Form.Control
+                    type="text"
+                    size="lg"
+                    placeholder="Email address"
+                  />
+                </Form.Group>
 
-          {/*Password (Hidden to User)*/}
-          <Form.Group className="Section3">
-            <Form.Control
-              type="password"
-              size="lg"
-              placeholder="Password"
-            />
-          </Form.Group>
+                {/*Password (Hidden to User)*/}
+                <Form.Group className="Section3">
+                  <Form.Control
+                    type="password"
+                    size="lg"
+                    placeholder="Password"
+                  />
+                </Form.Group>
 
 
-          {/*Register Button, links to Homepage for now*/}
-          <div className="SubmitButton">
-            <ButtonContained
-              variant="contained"
-              color="primary"
-              href="homepage">
-              Create Account
-            </ButtonContained>
-          </div>
+                {/*Register Button, links to Homepage for now*/}
+              <ButtonGroup className="SubmitButton">
+                  <ButtonContained
+                    variant="contained"
+                    color="primary"
+                    href="homepage">
+                    Create Account
+                  </ButtonContained>
 
-        </Form>
+              </ButtonGroup>
+
+              </Form>
+              </Col>
+              <Col md={5} sm={12} xs={12}>
+                <Image src={logo}className="DrawingLogin"/>
+              </Col>
+         </Row>
+        </Container>
       </div>
     </React.Fragment>
   );
