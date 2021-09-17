fc3ff6a3367bfa861ad0388bc97fbc284ad16f35
Anthony Vu <53590640+Thouny@users.noreply.github.com>
2021-09-17 19:32:08 +1000

    Small components tweaks

--- mern/client/src/components/ButtonText.js
+++ mern/client/src/components/ButtonText.js
@@ -2,16 +2,21 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import styles from "../styles/button.module.css";
 
-const ButtonText = ({ children, onClick, isDisabled }) => {
+const ButtonText = ({ children, onClick, isDisabled, type }) => {
   /**
    * @param {String} children The button label
    * @param {Function void} onClick Method that gets called when the user clicks on the button
    * @param {bool} isDisabled Whether the button is disabled or not
+   * @param {String} type Button's type. Can be either "button" or "submit"
    */
   return (
     <React.Fragment>
       {/* 
       If {isDisabled} is true, the button is showed as disabled
+      */}
+      {/* 
+      If nothing is given, {type} will be "button" by default. 
+      For button in forms, use "submit" as {type}
       */}
       <Button
         variant="link"
@@ -19,6 +24,7 @@ const ButtonText = ({ children, onClick, isDisabled }) => {
         onClick={onClick}
         className={styles.btn}
         disabled={isDisabled ? "true" : null}
+        type={type != null ? type : "button"}
       >
         {children}
       </Button>
