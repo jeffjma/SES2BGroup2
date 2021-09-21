9cfc473c7afba9b9d2b82643c716d91df654655f
13556042 <62224240+13556042@users.noreply.github.com>
2021-09-22 00:16:03 +1000

    Updated subject schema to have a name.

--- mern/server/Models/subject.js
+++ mern/server/Models/subject.js
@@ -31,6 +31,10 @@ const QuestionSchema = new Schema ({
 
 //Create Schema
 const SubjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     staff_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users',
