e450ab90dee5519f54df4f9e4b82f31e26c4d101
Beatrix Austria <baustria2108@gmail.com>
2021-09-24 23:41:37 +1000

    fixed css, haven't made the radio+button disable/enable work yet

--- mern/client/src/pages/assessment/Assessment.js
+++ mern/client/src/pages/assessment/Assessment.js
@@ -1,79 +1,85 @@
 import React from "react";
-import {
-  ButtonGroup,
-  Form
-} from "react-bootstrap";
+import { ButtonGroup, Form } from "react-bootstrap";
 import ButtonContained from "../../components/ButtonContained";
 import NavigationBar from "../../components/NavigationBar.js";
 import "./Assessment.css";
+import $ from 'jquery';
 
 const Assessment = () => {
   
+  $("input:radio").change(function () {$("#submit").prop("isDisabled", null);});
+
   return (
     <React.Fragment>
+      <NavigationBar />
       <div className="assessment-parent">
-          <NavigationBar />
-          <h3>(Subject) Test</h3>
-          <h4>Question: 1</h4>
-          <h5>Question Is Here?</h5>
-          <Form>
-            <div class="form-check">
-              <input 
-                 class="form-check-input" 
-                 type="radio" 
-                 name="answer1" 
-                 id="assessment1" 
-                 value="option1" />
-              <label class="form-check-label" for="assessment1">
-                Answer 1
-              </label>
-            </div>
+        <h3>(Subject) Test</h3>
+        <h4 className="questionTitle">Question 1: </h4>
+        <h5 className="questionDesc">Question Is Here?</h5>
+        <Form className="multipleChoice">
+          <div className="form-check">
+            <input
+              class="form-check-radio"
+              type="radio"
+              name="answer"
+              id="assessment1"
+              value="option1"
+              required
+            />
+            <label class="form-check-label" for="assessment1">
+              <h5>Answer 1</h5>
+            </label>
+          </div>
 
-            <div class="form-check">
-              <input 
-                 class="form-check-input" 
-                 type="radio" 
-                 name="answer2" 
-                 id="assessment2" 
-                 value="option2" />
-              <label class="form-check-label" for="assessment2">
-                Answer 2
-              </label>
-            </div>
+          <div className="form-check">
+            <input
+              class="form-check-radio"
+              type="radio"
+              name="answer"
+              id="assessment2"
+              value="option2"
+              required
+            />
+            <label class="form-check-label" for="assessment2">
+            <h5>Answer 2</h5>
+            </label>
+          </div>
 
-            <div class="form-check">
-              <input 
-                 class="form-check-input" 
-                 type="radio" 
-                 name="answer3" 
-                 id="assessment3" 
-                 value="option3" />
-              <label class="form-check-label" for="assessment3">
-                Answer 3
-              </label>
-            </div> 
+          <div className="form-check">
+            <input
+              class="form-check-radio"
+              type="radio"
+              name="answer"
+              id="assessment3"
+              value="option3"
+              required
+            />
+            <label class="form-check-label" for="assessment3">
+              <h5>Answer 3</h5>
+            </label>
+          </div>
 
-            <div class="form-check">
-              <input 
-                 class="form-check-input" 
-                 type="radio" 
-                 name="answer4" 
-                 id="assessment4" 
-                 value="option4" />
-              <label class="form-check-label" for="assessment4">
-                Answer 4
-              </label>
-            </div>
-
-          </Form>
-          <ButtonGroup>
-            <ButtonContained>
-              Next
-            </ButtonContained>
-          </ButtonGroup>
-          
+          <div className="form-check">
+            <input
+              class="form-check-radio"
+              type="radio"
+              name="answer"
+              id="assessment4"
+              value="option4"
+              required
+            />
+            <label className="form-check-label" for="assessment4">
+              <h5>Answer 4</h5>
+            </label>
+          </div>
+        </Form>
+        <ButtonGroup className="button" >
+          <ButtonContained id="submit">Next</ButtonContained>
+        </ButtonGroup>
       </div>
+      
     </React.Fragment>
+    
   );
 };
 
