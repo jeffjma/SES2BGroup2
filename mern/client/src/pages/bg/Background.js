b7262f3b7519cb09ed4f983b5ddc7a61afae2db6
Solacx <junyong6@gmail.com>
2021-09-29 01:27:34 +1000

    Basic UI.
    UI is setup and can be changed to demo version quick. Do soon, once I
    get answers to a couple questions.
    
    TODO:
     - Update database
     - Use cleaner approach to showing alternative questions

--- mern/client/src/pages/bg/Background.js
+++ mern/client/src/pages/bg/Background.js
@@ -1,23 +1,144 @@
-import { Container, Form } from "react-bootstrap";
+import React, { Component } from 'react';
+import { Row } from 'react-bootstrap';
+import ButtonContained from "../../components/ButtonContained";
+import "./Background.css";
 
 
-const Background = () => {
-  return (
-    <div>
-      <Container>
-      <Form>
-        <Form.Group>
-          <Form.Label>My current education level is...</Form.Label>
-          <Form.Select>
-            <option disabled>e.g. Tertiary Education - University</option>
-            <option value="2">Secondary Education</option>
-            <option value="3">Tertiary Education</option>
-          </Form.Select>
-        </Form.Group>
-      </Form>
-      </Container>
-    </div>
-  );
+class Background extends Component {
+
+  constructor() {
+    super();
+
+    this.sectionToRender = null;
+
+    this.renderAppropriateSection = this.renderAppropriateSection.bind(this);
+    this.renderHSSection = this.renderHSSection.bind(this);
+    this.renderUniSection = this.renderUniSection.bind(this);
+  }
+
+  /**
+   * Render appropriate section depending on selection in 'Past
+   * Education' input.
+   */
+  renderAppropriateSection(selection) {
+    this.sectionToRender = "";
+    this.forceUpdate();
+
+    switch (selection.toUpperCase()) {
+      case "SECONDARY":
+        this.sectionToRender = this.renderHSSection();
+        break;
+
+      case "TERTIARY":
+        this.sectionToRender = this.renderUniSection();
+        break;
+
+      default:
+        break;
+    }
+
+    this.forceUpdate();
+    console.log(this.sectionToRender);
+  }
+
+  renderHSSection() {
+    return (
+      <div>
+        <Row>
+          <SelectBox label="Year" options={["7", "8", "9", "10", "11", "12"]}></SelectBox>
+        </Row>
+        <Row>
+          <SelectBox label="Current Subjects" options={["English Advanced", "Etc."]}></SelectBox>
+        </Row>
+      </div>
+    );
+  }
+
+  renderUniSection() {
+    return (
+      <div>
+        <Row>
+          <SelectBox label="Year" options={["1", "2", "3", "4", "5"]}></SelectBox>
+        </Row>
+        <Row>
+          <SelectBox label="Semester" options={["1", "2"]}></SelectBox>
+        </Row>
+        <Row>
+          <SelectBox label="Faculty" options={["FEIT"]}></SelectBox>
+        </Row>
+        <Row>
+          <SelectBox label="Completed Subjects" options={["DSA", "Etc."]}></SelectBox>
+        </Row>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div class="container bg-main-container">
+        <Row>
+          <span class="bg-title">Your Background</span>
+        </Row>
+        <Row>
+          <SelectBox label="Past Education" options={["Secondary", "Tertiary"]} callback={ this.renderAppropriateSection }></SelectBox>
+        </Row>
+
+        { this.sectionToRender }
+
+        <ButtonContained variant="contained" color="primary" href="homepage">
+          Save
+        </ButtonContained>
+      </div>
+    );
+  }
+}
+
+/**
+ * Basic selection menu.
+ * 
+ * Use to create a selection menu with corresponding label positioned
+ * above it. Select is shown as the default option. Can be assigned a
+ * callback to run when value changes.
+ */
+class SelectBox extends Component {
+
+  constructor(props) {
+    super();
+
+    this.value = null;
+    this.label = props.label;
+    this.options = props.options;
+    this.callback = props.callback;
+
+    // Running in STRICT mode causes constructor to run twice.
+    if (this.options[0] !== "Select") {
+      this.options.unshift("Select");
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.value = event.target.value;
+    if (this.callback) {
+      this.callback(this.value);
+    }
+  }
+
+  render() {
+    return (
+      <div class="container sb-main-container">
+        <Row>
+          <label class="sb-label">{this.label}</label>
+          <div class="sb-select-container">
+            <select onChange={ this.onChange }>
+              { [...Array(this.options.length)].map((e, i) => <option>{this.options[i]}</option>) }
+            </select>
+          </div>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Background;
