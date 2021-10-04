49e64a3162e5742490a7e8c873974e045989bbf2
Homer Martillano <13537675Z@student.uts.edu.au>
2021-10-04 20:02:14 +1100

    fixed css/image, TBF

--- mern/client/src/pages/preassessment/PreAssessment.js
+++ mern/client/src/pages/preassessment/PreAssessment.js
@@ -2,6 +2,7 @@ import React from "react";
 import NaviBar from "../../components/NavigationBar";
 import ButtonOutlined from "../../components/ButtonOutlined"
 import { Link } from "react-router-dom";
+import logo_preAssessment from "../../assets/logo_preAssessment.png";
 import "./PreAssessment.css";
 
 const PreAssessment = () => {
@@ -9,43 +10,47 @@ const PreAssessment = () => {
         <React.Fragment>
             <NaviBar></NaviBar>
 
-            <div className="title">
-                <h4>Assessment Information</h4>
+            <div className="body-main">
+                <div className="title">
+                    <p>Assessment Information</p>
+                </div>
+
+                <div className="body-container">
+                    <div className="titleItem">
+                        <p>Title:</p>
+                    </div>
+                    <div className="maxPointsItem">
+                        <p>Max Points Obtainable:</p>
+                    </div>
+                    <div className="availableItem">
+                        <p>Available:</p>
+                    </div>
+                    <div className="timeItem">
+                        <p>Time Limit:</p>
+                    </div>
+                    <div className="attemptsItem">
+                        <p>Allowed Attempts:</p>
+                    </div>
+                    <div className="descriptionItem">
+                        <p>Description:</p>
+                    </div>
+                    <div className="beginButton">
+                        <Link to=''>
+                        <ButtonOutlined
+                            variant="contained"
+                            color="primary"
+                            href="">
+                            BEGIN ASSESSMENT
+                        </ButtonOutlined>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="image">
+                    <img src={logo_preAssessment} alt="logo"></img>
+                </div>
             </div>
 
-            <div className="body-container">
-                <div className="titleItem">
-                    <p>Title:</p>
-                </div>
-                <div className="maxPointsItem">
-                    <p>Max Points Obtainable:</p>
-                </div>
-                <div className="availableItem">
-                    <p>Available:</p>
-                </div>
-                <div className="timeItem">
-                    <p>Time Limit:</p>
-                </div>
-                <div className="attemptsItem">
-                    <p>Allowed Attempts:</p>
-                </div>
-                <div className="descriptionItem">
-                    <p>Description:</p>
-                </div>
-                <div className="beginButton">
-                    <Link to=''>
-                    <ButtonOutlined
-                        variant="contained"
-                        color="primary"
-                        href="">
-                        BEGIN ASSESSMENT
-                    </ButtonOutlined>
-                    </Link>
-                </div>
-            </div>
-
-            <div className="image">
-            </div>
         </React.Fragment>
     );
 };
