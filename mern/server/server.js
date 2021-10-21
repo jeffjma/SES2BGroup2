2e491ef3d3747daceeb98a326bc4ef79c25bb71f
Danielfont <danielplafontaine@gmail.com>
2021-10-22 00:56:33 +1100

    Added award api

--- mern/server/server.js
+++ mern/server/server.js
@@ -10,6 +10,7 @@ const subjects = require("./routes/api/subjects");
 const results = require("./routes/api/results");
 const test = require("./routes/api/tests");
 const exp = require("./routes/api/exponential");
+const awards = require("./routes/api/awards");
 
 const app = express();
 app.use(cors({ origin: "http://localhost:3000", withoutPreflight: true, credentials: true }));
@@ -43,6 +44,7 @@ app.use("/api/subjects", subjects);
 app.use("/api/results", results);
 app.use("/api/tests", test);
 app.use("/api/exponential", exp);
+app.use("/api/awards", awards);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
