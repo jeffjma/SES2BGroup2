970862ad061f2969d6af161c9252f5a3febd3ba5
13196530 <13196530@student.uts.edu.au>
2021-10-24 12:19:27 +1100

    fixed user model, fixed award model, routes on user model should now be working

--- mern/server/Models/User.js
+++ mern/server/Models/User.js
@@ -43,14 +43,14 @@ const UserSchema = new Schema({
     type: [String],
     required: false
   },
-  currentSubjects: {
+  awards: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: "awards",
     required: false
   },
   testResults: {
     type: [mongoose.Schema.Types.ObjectId],
-    ref: "tests",
+    ref: "results",
     required: false
   }
 });
