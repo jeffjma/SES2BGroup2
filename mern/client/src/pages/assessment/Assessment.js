c2a700d5ef4a1f94df66c21c8e42e23ef5f8b7fa
13556042 <62224240+13556042@users.noreply.github.com>
2021-10-26 01:08:59 +1100

    Setup cookie access.

--- mern/client/src/pages/assessment/Assessment.js
+++ mern/client/src/pages/assessment/Assessment.js
@@ -1,90 +1,119 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { ButtonGroup } from "react-bootstrap";
 import ButtonContained from "../../components/ButtonContained";
 import NavigationBar from "../../components/NavigationBar.js";
+import { withCookies, Cookies } from 'react-cookie';
+import { instanceOf } from 'prop-types';
 import "./Assessment.css";
 import Timer from "./Timer.js";
 import Question from "./attributes/Question";
 import Answer from "./attributes/Answer"
 import { Homepage } from "../Routes";
 
-export default class Assessment extends Component {
+const api = axios.create({
+  baseURL: `http://localhost:5000/api/users/profile`
+})
+
+class Assessment extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+
+  constructor(props){
+    super(props)
+    const { cookies } = props;
+    this.state = {
+      userID: cookies.get('userid'),
+      question: {
+        1:'What is 2+2?',
+        2:'What colour is the sky?',
+        3:'Between music, theater, and chariot racing, which sport did Nero win when he participated in the Olympics?',
+        4:'If angle C is 28 degrees, and angles A + D are equal to 88 degrees, what is the angle of B + E?',
+        5:'What’s one of the origins for the phrase “cat got your tongue”?',
+        6:'If we use “three watermelons in the sun” to visualize a certain matter’s size against the universe’s, what are the melons?',
+        7:'Counting both black and white surfaces, how many surfaces are there in total on a soccer ball?',
+      },
+      answers: {
+        1: {
+          1:'2',
+          2:'3',
+          3:'4',
+          4:'1'
+        },
+
+        2: {
+          1:'Hot Pink',
+          2:'Light Blue',
+          3:'Blood Red',
+          4:'Lime Green'
+        },
+
+        3: {
+          1:'Music',
+          2:'Theatre', 
+          3:'Chariot Racing', 
+          4:'All of them'
+        },
+
+        4: {
+          1:'74',
+          2:'64', 
+          3:'62', 
+          4:'40'
+        },
+
+        5: {
+          1:'Wild cats who don’t meow',
+          2:'Tongues are the same texture as a cat’s skin', 
+          3:'A breed of cat with no tongue', 
+          4:'Cats eating human tongues'
+        },
+
+        6: {
+          1:'Stars',
+          2:'Moons', 
+          3:'Galaxies', 
+          4:'People'
+        },
+
+        7: {
+          1:'26',
+          2:'34', 
+          3:'32', 
+          4:'22'
+        }, 
+      },
+      correctAnswers: {
+        1:'3',
+        2:'2',
+        3:'4',
+        4:'2',
+        5:'4',
+        6:'1',
+        7:'3'
+      },
+      correctAnswer: 0,
+      chosenAnswer: 0,
+      questionNumber: 1,
+      studentScore:0,
+      UserName: 'John Smith',   
+    }
+  }
 
 // dummy code, i had a seperate .js file before but too much of a hassle to use it. i just put it in here knowing it'll be gone
-  state = {
-        question: {
-          1:'What is 2+2?',
-          2:'What colour is the sky?',
-          3:'Between music, theater, and chariot racing, which sport did Nero win when he participated in the Olympics?',
-          4:'If angle C is 28 degrees, and angles A + D are equal to 88 degrees, what is the angle of B + E?',
-          5:'What’s one of the origins for the phrase “cat got your tongue”?',
-          6:'If we use “three watermelons in the sun” to visualize a certain matter’s size against the universe’s, what are the melons?',
-          7:'Counting both black and white surfaces, how many surfaces are there in total on a soccer ball?',
-        },
-        answers: {
-          1: {
-            1:'2',
-            2:'3',
-            3:'4',
-            4:'1'
-          },
+  
 
-          2: {
-            1:'Hot Pink',
-            2:'Light Blue',
-            3:'Blood Red',
-            4:'Lime Green'
-          },
-
-          3: {
-            1:'Music',
-            2:'Theatre', 
-            3:'Chariot Racing', 
-            4:'All of them'
-          },
-
-          4: {
-            1:'74',
-            2:'64', 
-            3:'62', 
-            4:'40'
-          },
-
-          5: {
-            1:'Wild cats who don’t meow',
-            2:'Tongues are the same texture as a cat’s skin', 
-            3:'A breed of cat with no tongue', 
-            4:'Cats eating human tongues'
-          },
-
-          6: {
-            1:'Stars',
-            2:'Moons', 
-            3:'Galaxies', 
-            4:'People'
-          },
-
-          7: {
-            1:'26',
-            2:'34', 
-            3:'32', 
-            4:'22'
-          }, 
-        },
-        correctAnswers: {
-          1:'3',
-          2:'2',
-          3:'4',
-          4:'2',
-          5:'4',
-          6:'1',
-          7:'3'
-        },
-        correctAnswer: 0,
-        chosenAnswer: 0,
-        questionNumber: 1,
-        studentScore:0,
-        UserName: 'John Smith',   
+  componentDidMount(){
+    api.post('/', {
+      userID: this.state.userID
+   })
+    .then(res => {
+        console.log(res.data)
+        this.setState({ 
+            UserName: res.data.name,
+        })
+    })
   }
 
 //checks if answers are correct and adds to student score 
@@ -175,3 +204,5 @@ export default class Assessment extends Component {
     );
   }
 }
+
+export default withCookies(Assessment);
