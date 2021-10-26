27a62935ef1291442d31c9d9d10e8824c830e910
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2021-10-26 21:05:39 +1100

    correct subjectID used in examiners subject page

--- mern/client/src/pages/examinerhome/ExaminerHome2.js
+++ mern/client/src/pages/examinerhome/ExaminerHome2.js
@@ -21,106 +21,20 @@ class ExaminerHome2 extends Component {
       //this state values are now only for test before fetching data from api
       userID: cookies.get('userid'),
       subjectName: "",
-      subjectId: "616abaf8c1bb94f6986db37e",
-      assessments: [
-        
-        // All Subjects (which is for test only before Fetching data from database)
-        // {
-        //   id: "1",
-        //   name: "Javascript test",
-        //   studentCount: 56,
-        //   assessmentCount: 4,
-        //   status: "1",
-        //   chartData: {
-        //     labels: ["Beginner", "Intermediate", "Advanced", "Expert"],
-        //     datasets: [
-        //       {
-        //         label: "# of Attempts",
-        //         data: [12, 19, 3, 5],
-        //         backgroundColor: [
-        //           "rgba(200, 77, 99, 1)",
-        //           "rgba(255, 232, 112, 1)",
-        //           "rgba(253, 165, 125, 1)",
-        //           "rgba(91, 194, 167, 1)",
-        //         ],
-        //         borderColor: [
-        //           "rgba(200, 77, 99, 1)",
-        //           "rgba(255, 232, 112, 1)",
-        //           "rgba(253, 165, 125, 1)",
-        //           "rgba(91, 194, 167, 1)",
-        //         ],
-        //         borderWidth: 1,
-        //         hoverOffset: 4,
-        //       },
-        //     ],
-        //   },
-        // },
-        // {
-        //   id: "2",
-        //   name: "Javascript test",
-        //   studentCount: 56,
-        //   assessmentCount: 4,
-        //   status: "1",
-        //   chartData: {
-        //     labels: ["Beginner", "Intermediate", "Advanced", "Expert"],
-        //     datasets: [
-        //       {
-        //         label: "# of Attempts",
-        //         data: [12, 19, 3, 5],
-        //         backgroundColor: [
-        //           "rgba(200, 77, 99, 1)",
-        //           "rgba(255, 232, 112, 1)",
-        //           "rgba(253, 165, 125, 1)",
-        //           "rgba(91, 194, 167, 1)",
-        //         ],
-        //         borderColor: [
-        //           "rgba(200, 77, 99, 1)",
-        //           "rgba(255, 232, 112, 1)",
-        //           "rgba(253, 165, 125, 1)",
-        //           "rgba(91, 194, 167, 1)",
-        //         ],
-        //         borderWidth: 1,
-        //         hoverOffset: 4,
-        //       },
-        //     ],
-        //   },
-        // },
-        // {
-        //   id: "3",
-        //   name: "Javascript test",
-        //   studentCount: 56,
-        //   assessmentCount: 4,
-        //   status: "1",
-          // chartData: {
-          //   labels: ["Beginner", "Intermediate", "Advanced", "Expert"],
-          //   datasets: [
-          //     {
-          //       label: "# of Attempts",
-          //       data: [12, 19, 3, 5],
-          //       backgroundColor: [
-          //         "rgba(200, 77, 99, 1)",
-          //         "rgba(255, 232, 112, 1)",
-          //         "rgba(253, 165, 125, 1)",
-          //         "rgba(91, 194, 167, 1)",
-          //       ],
-          //       borderColor: [
-          //         "rgba(200, 77, 99, 1)",
-          //         "rgba(255, 232, 112, 1)",
-          //         "rgba(253, 165, 125, 1)",
-          //         "rgba(91, 194, 167, 1)",
-          //       ],
-          //       borderWidth: 1,
-          //       hoverOffset: 4,
-          //     },
-          //   ],
-          // },
-        // },
-      ],
+      subjectId: "",
+      assessments: [],
     };
   }
   
   componentDidMount(){
 
+    if(this.props.location?.state != null) {
+      console.log(this.props.location?.state )
+      this.setState({
+        subjectId: this.props.location?.state?.subjectID.subID,
+        subjectName: this.props.location?.state?.subjectName.subname
+      })
+  
     axios.post('http://localhost:5000/api/users/profile', {
       userID: this.state.userID
     })
@@ -128,12 +42,11 @@ class ExaminerHome2 extends Component {
         console.log(res.data)
         this.setState({ 
             UserName: res.data.name,
-            subjectName: res.data.subjectName,
         })
     });
 
     axios.post("http://localhost:5000/api/tests/getForSubject", {
-      subject: this.state.subjectId
+      subject: this.props.location?.state?.subjectID.subID
     })
     .then(res => {
       console.log(res.data);
@@ -174,6 +87,8 @@ class ExaminerHome2 extends Component {
         this.forceUpdate();
       }
     })
+
+    }
   }
 
   redirectAddTest() {
